6380cef3b7699890f059721d99c2b23d47a16e5e
Sankalp-sa <sankalp.p@ahduni.edu.in>
2022-12-21 14:07:26 +0530

    added news api and new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './Components/NavBar';
+import News from './Components/News';
+import NewsItem from './Components/NewsItem';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <NavBar/>
+        <News/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
       </div>
     )
   }
